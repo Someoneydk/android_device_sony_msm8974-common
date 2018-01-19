e885282963027893970554f2cdc989dfad637613
Someoneydk <yt.sahulkrishan@gmail.com>
2018-01-19 17:51:39 +0100

    Another batch of removing unwanted text... sigh...

--- Android.mk
+++ Android.mk
@@ -3,9 +3,6 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(BOARD_VENDOR),sony)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
-<<<<<<< HEAD
-=======
     include hardware/qcom/gps/sdm845/Android.mk
->>>>>>> 9e53c40... sony-8974: GPS: Update GPS HAL to sdm845 and loc_api.
 endif
 endif
