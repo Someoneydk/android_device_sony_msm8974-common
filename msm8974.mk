080cc16af48f6f89e3575fc98ea2f36673f7a6e2
Someoneydk <yt.sahulkrishan@gmail.com>
2018-03-20 21:58:38 +0100

    msm8974-common: Add dummy android.hidl.base@1.0

--- msm8974.mk
+++ msm8974.mk
@@ -80,6 +80,11 @@ PRODUCT_PACKAGES += \
     libloc_ds_api \
     liblocation_api
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0-java
+
 # Ion
 PRODUCT_PACKAGES += \
     libion
